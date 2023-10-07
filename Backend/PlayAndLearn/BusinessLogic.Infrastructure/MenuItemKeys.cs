86b29e92b18a884c970ae4f966df1f5d1e85dfef
Manuel Peise <manuel.p80@gmx.de>
2023-10-07 10:58:54 +0200

    add-logic-to get-side-menu-entries

---
+++ Backend/PlayAndLearn/BusinessLogic.Infrastructure/MenuItemKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLogic.Infrastructure
+{
+    internal class MenuItemKeys
+    {
+        public const string AppTitleKey = "appTitleKey";
+
+        public const string MenuItemGamesKey = "itemGames";
+        public const string MenuMemoryKey = "itemMemory";
+        public const string MenuTicTacToeKey = "itemTicTacToe";
+
+    }
+}
---
+++ Backend/PlayAndLearn/Service.Infrastructure/Controllers/SideMenuController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic.Shared.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models.Infrastructure;
+
+namespace Service.Infrastructure.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SideMenuController: ControllerBase
+    {
+        private readonly ISideMenuHandler _handler;
+
+        public SideMenuController(ISideMenuHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpGet(Name = "GetPublicMenu")]
+        public async Task<SideMenu> GetPublicMenu()
+        {
+            return await _handler.GetPublicMenu();
+        }
+    }
+}
